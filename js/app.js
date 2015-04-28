6b31c5dbe82eca2302ce3b2d0ed871e29dcd63eb
Daniel Treasure <dbtreasure@gmail.com>
2015-04-27 18:20:00 -0600

    sets up basic routing with ui-router

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
-var proef = angular.module('proef', []);
+var proef = angular.module('proef', ['ui.router']);
+
+proef.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "../views/home.html"
+    })
+    .state('approach', {
+      url: "/approach",
+      templateUrl: "../views/approach.html"
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "../views/about.html"
+    })
+    .state('contact', {
+      url: "/contact",
+      templateUrl: "../views/contact.html"
+    })
+});
 
 proef.controller('mainCtrl', ['$scope', function($scope) {
   $scope.currentBackground = 1;
