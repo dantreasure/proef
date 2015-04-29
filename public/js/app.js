d5a723c4313ea4d091395aed8769760cbaa27f57
Daniel Treasure <dbtreasure@gmail.com>
2015-04-28 21:45:15 -0600

    cleanup on aisle app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,8 @@ var proef = angular.module('proef', ['ui.router']);
 var mouseEvt;
 
 proef.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
   $urlRouterProvider.otherwise("/");
-  //
-  // Now set up the states
+
   $stateProvider
     .state('home', {
       url: "/",
