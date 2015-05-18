7b41e271989c4b614555485ac9f896d0aececbc9
Daniel Treasure <dbtreasure@gmail.com>
2015-05-18 15:24:27 -0600

    Working drawers for approach but not perfect

--- src/js/approach-ctrl.js
+++ src/js/approach-ctrl.js
@@ -1,3 +1,35 @@
-proef.controller('approachCtrl', ['$scope', function($scope) {
-	$scope.message = 'this is the approach page';
+proef.controller('approachCtrl', ['$scope','$timeout', function($scope, $timeout) {
+	$scope.articles = {
+		whatWeDo: 'closed',
+		howWeWork: 'closed',
+		whatWeOffer: 'closed',
+		aesthetics: 'closed',
+		clients: 'closed'
+	};
+
+	$scope.activeArticle = function(article){
+		if($scope.articles[article] === 'open' || $scope.articles[article] === 'transition'){
+			return true
+		} else if ($scope.articles[article]=== 'closed'){
+			return false
+		}
+	}
+
+	$scope.articleToggle = function(article){
+		var setToOpen = function(){
+			$scope.articles[article] = 'open';
+		};
+
+		var setToClose = function(){
+			$scope.articles[article] = 'closed';
+		}
+
+		if($scope.articles[article] === 'closed'){
+			$scope.articles[article] = 'transition';
+			$timeout(setToOpen, 1000);
+		} else{
+			$scope.articles[article] = 'transition';
+			$timeout(setToClose, 1000);
+		}
+	}
 }]);
